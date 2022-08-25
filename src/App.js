28851f439d0262d7818fcab40bd4e3478168bab3
jeanpaulHamuli <jeanpaulhamuli@gmail.com>
2022-08-25 11:29:22 +0200

    Initialize the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setUserPassword] = useState('');
+  const [message, setMessage] = useState(null);
+  
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  }
+
+
+  const getPassword = (e) => {
+    setUserPassword(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    if(username.length > 0) { 
+      setMessage('Welcome ' + username);
+    } else {
+      setMessage('Please enter a username');
+    }
+    // if(username === 'admin' && password === 'admin') {
+    //   setMessage('Login Successful');
+    // }else {
+    //   setMessage('Login Failed');
+    // }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TEST</h1>
+      {message ? <p style={{color: 'red'}}>{message}</p> : null}
+
+      <form style={{display: 'flex', flexDirection: 'column', width: '20%', margin: 'auto'}}>
+        <input type="text" placeholder="Name" name='username' onChange={handleUsername} style={{marginBottom: '10px'}} />
+        <input type="password" placeholder="Password"  name='password' onChange={getPassword} style={{marginBottom: '10px'}} />
+        <button type="submit" onClick={handleSubmit}>Submit</button>
+      </form>
     </div>
   );
 }
