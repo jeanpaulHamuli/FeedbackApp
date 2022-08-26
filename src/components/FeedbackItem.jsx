c22b00aa5b3fb830782adbd5177f1bcfa0433569
jeanpaulHamuli <jeanpaulhamuli@gmail.com>
2022-08-26 10:16:41 +0200

    Feedback List (Props and loop)

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,22 +1,12 @@
-import { useState } from 'react'
 
-export default function FeedbackItem() {
-  const [rating, setRating] = useState(7);
-  const [text, setText] = useState('This is an example of a feedback item');
-
-  const handleClick = () => {
-    setRating((prev) => {
-        return prev + 1;
-    })
-  }
+export default function FeedbackItem({item}) {
 
   return (
     <div className="card">
-      <div className="num-display">{rating}</div>
+      <div className="num-display">{item.rating}</div>
       <div className="text-display">
-        {text}
+        {item.text}
       </div>
-      <button onClick={handleClick} >CLick</button>
     </div>
   )
 }
